04a3a134862175ca227d76d85e8bcd9f5261912b
DaltonHart <daltonvaren@gmail.com>
2020-10-30 10:43:51 -0700

    adds starting prompts

--- TrippleX.cpp
+++ TrippleX.cpp
@@ -1,4 +1,10 @@
+#include <iostream>
+
 int main()
 {
+  std::cout << "You are a secret agent breaking into ta secure server room...";
+  std::cout << std::endl;
+  std::cout << "Enter the correct code to contine...";
+  std::cout << std::endl;
   return 0;
 }
