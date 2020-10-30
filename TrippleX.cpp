8ce0c2566a03f6b04587ee1c372a6215d1f0a03f
DaltonHart <daltonvaren@gmail.com>
2020-10-30 10:52:53 -0700

    adds example of initializing and printing variables

--- TrippleX.cpp
+++ TrippleX.cpp
@@ -2,9 +2,19 @@
 
 int main()
 {
-  std::cout << "You are a secret agent breaking into ta secure server room...";
-  std::cout << std::endl;
-  std::cout << "Enter the correct code to contine...";
-  std::cout << std::endl;
+  std::cout << "You are a secret agent breaking into ta secure server room..." << std::endl;
+
+  std::cout << "Enter the correct code to contine..." << std::endl;
+
+  int a = 4;
+  int b = 5;
+  int c = 6;
+
+  int sum = a + b + c;
+  int product = a * b * c;
+
+  std::cout << sum << std::endl;
+  std::cout << product << std::endl;
+
   return 0;
 }
