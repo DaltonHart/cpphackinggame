f5a35feb3d7dcd55bd32ccb6ba5a67c4683589bf
DaltonHart <daltonvaren@gmail.com>
2020-10-30 11:59:13 -0700

    adds split to seperate functions and ascii art

--- TrippleX.cpp
+++ TrippleX.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
 
-int main()
+void PrintIntro()
 {
+  std::cout << ".OOOOOOOOOOOOOOO @@                                   @@ OOOOOOOOOOOOOOOO.\nOOOOOOOOOOOOOOOO @ @ @ @OOOOOOOOOOOOOOOO\nOOOOOOOOOO'''''' @ @                                    @ @ ```````OOOOOOOOO\nOOOOO'' aaa @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @\"\"\"                   \"\"\"\"\"\"\"\"\"@@aaaa `OOOO\nOOOOO,\"\"\"\"@@@@@@@@@@@@@@\"\"\"\"                                     a@\"\" OOOA\nOOOOOOOOOoooooo,| OOoooooOOOOOS\nOOOOOOOOOOOOOOOOo,| OOOOOOOOOOOOC\nOOOOOOOOOOOOOOOOOO,| OOOOOOOOOOOOI\nOOOOOOOOOOOOOOOOOO @ | OOOOOOOOOOOOOI\nOOOOOOOOOOOOOOOOO '@                                        OOOOOOOOOOOOOOb\nOOOOOOOOOOOOOOO ' a '      | OOOOOOOOOOOOOy\nOOOOOOOOOOOOOO' '       aa`OOOOOOOOOOOP\n OOOOOOOOOOOOOOb, ..                                          `@aa``OOOOOOOh\n OOOOOOOOOOOOOOOOOOo                                           `@ @ @aa OOOOo\n OOOOOOOOOOOOOOOOOOO | @ @ @OOOOe\n OOOOOOOOOOOOOOOOOOO @aaaaaaa @ @',OOOOn\n OOOOOOOOOOOOOOOOOOO @aaa @ @ @ @ @ @ @ @\"\" @ @OOOOOi\n OOOOOOOOOO ~~aaaaaa\"a                 aaa@@@@@@@@@@\"\"            @@ OOOOOx\nOOOOOO aaaa @\"\"\"\"\"\"\"\" \"\"            @@@@@@@@@@@@\"\"               @@@|`OOOO'\nOOOOOOOo`@ @a aa @ @ @ @ @ @ @ @ @\"\"         a@        @@@@ OOOO9\nOOOOOOO'  `@@a               @@a@@   @@\"\"           a@@   a     |@@@ OOOO3\n`OOOO '       `@    aa@@       aaa\"\"\"          @a        a@     a@@@',OOOO\'\n";
+
   // prints to the terminal the welcome messages
-  std::cout << "You are a secret agent breaking into ta secure server room...\n";
+  std::cout
+      << "You are a secret agent breaking into ta secure server room...\n";
 
   std::cout << "Enter the correct code to contine...\n\n";
+}
+
+void PlayGame()
+{
+  PrintIntro();
 
   // Declare 3 number code
   const int CodeA = 4;
@@ -18,7 +26,7 @@ int main()
   // Print CodeSum and CodeProduct to the terminal
   std::cout << "There are 3 numbers in the code\n";
   std::cout << "\nThe codes add-up to: " << CodeSum;
-  std::cout << "\nThe codes multiply to give: " << CodeProduct;
+  std::cout << "\nThe codes multiply to give: " << CodeProduct << std::endl;
 
   // takes in player input and outputs said output
   int GuessA, GuessB, GuessC;
@@ -35,6 +43,10 @@ int main()
   {
     std::cout << "\nYou Lose!";
   }
+}
 
+int main()
+{
+  PlayGame();
   return 0;
 }
